02e2fd6a135e23093276142ee8b9e6a4f6438ee1
Bertolin <bertolb@8c8590019708.ant.amazon.com>
2018-01-03 15:08:21 +0000

    Added CLI parsing through kingpin.v2 library
    The CLI args previously parsed using golang's default flags library
    are now parsed using kingpin.v2. This results in a much more
    compact and resilient codebase:
      * main.go: Now contains the definition of the flags and triggers
        the flag parsing.
      * args.go: All code dedicated to parsing and understanding the
        flags has been replaced by a much simpler and efficient logic.

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
+)
+
+var (
+	app              = kingpin.New("stm", "A command-line telegram message sender.")
+	botApiKey        = app.Flag("bot", "Telegram bot API key").Short('b').String()
+	chatId           = app.Flag("chat", "Telegram chat ID").Short('c').String()
+	identityFilePath = app.Flag("file", "Path to identity CSV file containing the bot API key and the chat ID").Short('i').Default(os.Getenv("HOME")+"/.stm").String()
+	message          = app.Arg("msg", "Message to send").Required().String()
+)
+
 func main() {
-  botApiKey, chatId, textMessage := getParameters()
-  sendMessage(botApiKey, chatId, textMessage)
+  app.Parse(os.Args[1:])
+	botApiKey, chatId := getParameters(*botApiKey, *chatId, *identityFilePath)
+	sendMessage(botApiKey, chatId, *message)
 }
